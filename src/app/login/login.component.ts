486c64d45059996fcc8b6447172903f51c81c510
jacob <jakeytawil18@gmail.com>
2024-05-14 16:39:14 -0400

    form controls for auth components

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,25 +5,39 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatCardModule} from '@angular/material/card'
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
+    CommonModule,
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
     MatCardModule,
     RouterLink,
     RouterLinkActive,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
 
+  loginFormData: FormGroup
+  constructor(private formBuilder: FormBuilder) {
+    this.loginFormData = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
   handleSubmit(e:any){
-    console.log(e)
+    console.log(this.loginFormData.value)
+    this.loginFormData.reset()
   }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -5,6 +5,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatCardModule} from '@angular/material/card'
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -16,14 +17,24 @@ import { MatButtonModule } from '@angular/material/button';
     MatCardModule,
     RouterLink,
     RouterLinkActive,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+
+  signupFormData: FormGroup
+  constructor(private formBuilder: FormBuilder) {
+    this.signupFormData = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
   
   handleSubmit(e:any){
-    console.log(e)
+    console.log(this.signupFormData.value)
+    this.signupFormData.reset()
   }
 }
