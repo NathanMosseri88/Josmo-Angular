6b5b6995b37933c5ec35cf6c76759de2e1cb9f22
Nathan Mosseri <nathan@simpletowork.com>
2024-06-25 15:58:01 -0400

    auth guard added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,13 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { ProposalFormComponent } from './proposal-form/proposal-form.component';
 import { SignupComponent } from './signup/signup.component';
 import { ComparativeStyleComponent } from './comparative-style/comparative-style.component';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'signup', component: SignupComponent},
-    {path: 'proposal-form', component: ProposalFormComponent},
-    {path: 'styles-report', component: ComparativeStyleComponent},
-    {path: '', component: HomeComponent},
+    {path: 'proposal-form', component: ProposalFormComponent, canActivate: [AuthGuard]},
+    {path: 'styles-report', component: ComparativeStyleComponent, canActivate: [AuthGuard]},
+    {path: '', redirectTo: '/proposal-form', pathMatch: 'full'},
     {path: '**', component: NotFoundComponent}
 ];
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,32 @@
+// import { CanActivateFn } from '@angular/router';
+
+// export const authGuard: CanActivateFn = (route, state) => {
+//   return true;
+// };
+
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    
+    if (this.authService.isLoggedIn()) {
+      return true;
+    } else {
+      // Navigate to login page or any other page you want when user is not logged in
+      this.router.navigate(['/login']); // Example: Redirect to login page
+      return false;
+    }
+  }
+}
+
